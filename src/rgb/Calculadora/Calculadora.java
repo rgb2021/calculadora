ed4f4283fc3969fa813e870d4ace14b51526b567
rgb2021 <orgbo@icloud.com>
2021-05-23 17:58:34 +0200

    cambiando la implementación de raizCuadrada para que no use la librería math

--- src/rgb/Calculadora/Calculadora.java
+++ src/rgb/Calculadora/Calculadora.java
@@ -24,7 +24,24 @@ public class Calculadora
 	
 	public double raizCuadrada (int a )
 	{
-		return  Math.sqrt(a);
+		double aux = Double.NaN;
+
+		double resultado = a / 2;
+		
+		if (resultado > 0)
+		{
+
+			do {
+				aux = resultado;
+				resultado = (aux + (a / aux)) / 2;
+			} while ((aux - resultado) != 0);
+	
+			return resultado;
+		}
+		else
+		{
+			return Double.NaN;
+		}
 	}
 	
 
